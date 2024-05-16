957c5d0f40cff3f5de8547fc5756975b2bf8066f
Mitchell Hornsby <mitchellrh78@gmail.com>
2024-05-16 15:07:18 -0700

    Set up initial file structure

---
+++ app/home/page.tsx
@@ -0,0 +1,9 @@
+import Landing from "./sections/landing";
+
+export default function Home() {
+  return (
+    <main>
+      <Landing />
+    </main>
+  );
+}
---
+++ app/home/sections/landing/index.tsx
@@ -0,0 +1,7 @@
+export default function Landing() {
+    return (
+        <section>
+            <h1 className="bg-green-100 p-8">Template</h1>
+        </section>
+    )
+}
